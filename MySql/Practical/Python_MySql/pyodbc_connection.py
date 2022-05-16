1976597a3279400318d2392a24192c8da72edc1e
Saphall <safalsakha@gmail.com>
2022-05-16 21:00:38 +0545

    Format with black

--- MySql/Practical/Python_MySql/pyodbc_connection.py
+++ MySql/Practical/Python_MySql/pyodbc_connection.py
@@ -1,8 +1,18 @@
 import pyodbc
-server='SERVERNAME_HERE'
-database= 'DATABASE_NAME_HERE'
-username='USERNAME_HERE'
-password='PASSWORD_HERE'
-cnxn=pyodbc.connect('DRIVER={MySQL};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+
+server = "SERVERNAME_HERE"
+database = "DATABASE_NAME_HERE"
+username = "USERNAME_HERE"
+password = "PASSWORD_HERE"
+cnxn = pyodbc.connect(
+    "DRIVER={MySQL};SERVER="
+    + server
+    + ";DATABASE="
+    + database
+    + ";UID="
+    + username
+    + ";PWD="
+    + password
+)
 
 cursor = cnxn.cursor()
