90500750ee89f1ef4e8e9fe280524fbc9af62bce
Saphall <safalsakha@gmail.com>
2022-05-16 21:01:19 +0545

    Python & Postgresql connection

---
+++ Postgresql/Practical/Python_Postgres/psycopg_connection.py
@@ -0,0 +1,32 @@
+import psycopg2
+
+try:
+    connection = psycopg2.connect(
+        user="USERNAME_HERE",
+        password="PASSWORD_HERE",
+        host="HOST_HERE",
+        port="PORT_HERE",
+        database="DATABASE_HERE",
+    )
+    cursor = connection.cursor()
+
+    table_create_query = """
+    CREATE TABLE IF NOT EXISTS test(
+        id serial PRIMARY KEY,
+        first_name VARCHAR(30) NOT NULL,
+        last_name VARCHAR(30) NOT NULL,
+        dob DATE,
+        location VARCHAR(50),
+    );"""
+
+    cursor.execute(table_create_query)
+    connection.commit()
+
+except Exception as e:
+    print("[-] Exception Occurred:", e)
+
+finally:
+    print("[+] Executed !")
+    cursor.close()
+    connection.close()
+    print("[+] Connection Closed !")
