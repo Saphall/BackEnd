f6e563554f92c704f30aecd283f703c3e89ab136
Saphal Shakha <66344649+Saphall@users.noreply.github.com>
2022-06-11 22:39:14 +0545

    Update pyodbc_connection.py

--- MySql/Practical/Python_MySql/pyodbc_connection.py
+++ MySql/Practical/Python_MySql/pyodbc_connection.py
@@ -5,6 +5,7 @@ DATABASE = "DATABASE_NAME_HERE"  # test_database
 USERNAME = "USERNAME_HERE"  # user_name
 PASSWORD = "PASSWORD_HERE"  # password
 TABLENAME = "TABLE_NAME_HERE"  # test_table
+
 # Driver_info can be obtained by using `pyodbc-installer -d -l` command in terminal.
 cnxn = pyodbc.connect(
     "DRIVER={MySQL ODBC 8.0 Driver};SERVER="
